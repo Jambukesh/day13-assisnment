c91d76f8d7d3f04bb31e99fe4972b095ca2123bc
Jambukesh <jkesh360@gmail.com>
2023-11-22 16:38:50 +0530

    interface

---
+++ day13/day13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Smartphone s = new Smartphone("redme",2500);
+            Laptop pc = new Laptop("linux",11);
+            Console.WriteLine($"Smartphone connect{s.Connect()}");
+            Console.WriteLine($"Laptop connect{pc.Connect()}");
+            s.Charge(70);
+            pc.Charge(40);
+            Console.WriteLine($"Smartphone info:{s.Display()}");
+            Console.WriteLine($"Laptop info:{pc.Display()}");
+        }
+    }
+}
---
+++ day13/day13/Smartphone.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day13
+{
+    internal class Smartphone : IConnectable, IRechargeable, IDisplayable
+    {
+
+        string model;
+        int Rate;
+        int battery;
+
+        public Smartphone(string model, int rate )
+        {
+            this.model = model;
+           this.Rate = rate;
+            this.battery = 100;
+        }
+
+        public void Charge(int minutes)
+        {
+            
+                if(battery<100)
+            {
+                battery += minutes;
+                Console.WriteLine($"minutes to charging{minutes}");
+            }
+            else
+            {
+                Console.WriteLine("Battery full");
+            }
+        }
+
+        public bool Connect()
+        {
+            Console.WriteLine("connect as charging");
+            return true;
+        }
+
+        public string Display()
+        {
+            return $"{model}\t{Rate}\t{battery}";
+        }
+    }
+
+    }
+
